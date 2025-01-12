9439d9290ef4ce210a2badb7fed932f5556bfa96
jinoc <choiae369@gmail.com>
2025-01-12 21:41:27 +0900

    Chore: API 라우트의 response 객체 살펴보기
    - req의 method에 따라 status 변경

--- src/pages/api/short-links/index.js
+++ src/pages/api/short-links/index.js
@@ -1,3 +1,34 @@
 export default function handler(req, res) {
+  switch (req.method) {
+    case 'POST':
+      res.status(201).send({
+        title: '위키피디아 next.js',
+        url: 'https://en.wikipedia.org/wiki/Next.js',
+      });
+      break;
+    case 'GET':
+      res.send([
+        {
+          id: 'abc',
+          title: '위키피디아 next.js',
+          url: 'https://en.wikipedia.org/wiki/Next.js',
+        },
+        {
+          id: 'def',
+          title: '코드잇 자유게시판판',
+          url: 'https://codeit.kr/community/general',
+        },
+        {
+          id: 'ghi',
+          title: '코드잇 질문 답변',
+          url: 'https://codeit.kr/community/questions',
+        },
+      ]);
+      break;
+
+    default:
+      res.status(404).send();
+      break;
+  }
   res.status(200).send('hi next.js');
 }
