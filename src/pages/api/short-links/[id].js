60b785a6deda2b8793c8b1508b3948f80cafdf3c
jinoc <choiae369@gmail.com>
2025-01-13 23:07:10 +0900

    Feat: 도큐먼트 수정, 삭제 추가
    - short-links에 PATCH, DELETE 추가

--- src/pages/api/short-links/[id].js
+++ src/pages/api/short-links/[id].js
@@ -11,11 +11,18 @@ export default async function handler(req, res) {
 
   switch (req.method) {
     case 'PATCH':
-      res.send('short-link 수정');
+      const updatedShortLink = await ShortLink.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.send(updatedShortLink);
       break;
     case 'GET':
       const shortLink = await ShortLink.findById(id);
       res.send(shortLink);
+      break;
+    case 'DELETE':
+      await ShortLink.findByIdAndDelete(id);
+      res.status(204).send();
       break;
     default:
       res.send();
