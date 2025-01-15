6fbcb4a0d60691991f879979789ed0e9185d6502
jinoc <choiae369@gmail.com>
2025-01-15 21:24:10 +0900

    Feat: 짧은 주소 수정, 삭제 기능

--- src/pages/short-links/[id].js
+++ src/pages/short-links/[id].js
@@ -3,8 +3,38 @@ import ShortLinkForm, {
   ShortLinkFormType,
 } from '@/components/shortLinkForm/ShortLinkForm';
 import styles from '@/styles/ShortLinkEditPage.module.css';
+import dbConnect from '@/db/dbConnect';
+import ShortLink from '@/db/models/ShortLink';
+import axios from '@/lib/axios';
+import { useRouter } from 'next/router';
 
-export default function ShortLinkEditPage() {
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  await dbConnect();
+  const shortLink = await ShortLink.findById(id);
+
+  if (shortLink) {
+    return {
+      props: {
+        shortLinks: JSON.parse(JSON.stringify(shortLink)),
+      },
+    };
+  }
+
+  return {
+    notFound: true,
+  };
+}
+
+export default function ShortLinkEditPage({ shortLinks }) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  async function handleSubmit(values) {
+    await axios.patch(`/short-links/${id}`, values);
+    router.push('/short-links/');
+  }
+
   return (
     <>
       <Head>
@@ -12,7 +42,11 @@ export default function ShortLinkEditPage() {
       </Head>
       <div className={styles.page}>
         <h1 className={styles.title}>수정하기</h1>
-        <ShortLinkForm type={ShortLinkFormType.Edit} />
+        <ShortLinkForm
+          type={ShortLinkFormType.Edit}
+          initialValues={shortLinks}
+          onSubmit={handleSubmit}
+        />
       </div>
     </>
   );
